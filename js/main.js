90a14f93cae5879498293bf90b16cabcd85af01a
hexamidie <hexamidie@gmail.com>
2026-03-14 18:53:51 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('Selamat Menapaki Usia Baru, Romo Toto!').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
